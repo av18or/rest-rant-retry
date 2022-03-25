577651cccbe5bb7880f3ffa0a23d97e1c75d2cb4
Clayton <claytongorham@gmail.com>
2022-03-25 07:14:43 -0700

    add photos and grid to index page

--- controllers/places.js
+++ controllers/places.js
@@ -7,14 +7,14 @@ router.get("/", (req, res) => {
       city: "Seattle",
       state: "WA",
       cuisines: "Thai, Pan-Asian",
-      pic: "http://placekitten.com/250/250",
+      pic: "/images/dining1.jpg",
     },
     {
       name: "Coding Cat Cafe",
       city: "Phoenix",
       state: "AZ",
       cuisines: "Coffee, Bakery",
-      pic: "http://placekitten.com/250/250",
+      pic: "/images/dining2.jpg",
     },
   ];
   res.render("places/index", { places });

cf7e3299f57f3a7d2e33ffbf30f1eaafee20823e
Clayton <claytongorham@gmail.com>
2022-03-25 07:04:28 -0700

    add style

--- views/home.jsx
+++ views/home.jsx
@@ -3,12 +3,24 @@ const Def = require('./default')
 
 function home () {
     return (
-      <Def>
-          <main>
-              <h1>HOME</h1>
-          </main>
-      </Def>
+        <Def>
+            <main>
+    <h1>HOME</h1>
+    <div>
+      <img src="/images/food.jpg" alt="Food" />
+      <div>
+        Photo by <a href="https://unsplash.com/@lvnatikk">Lily Banse</a> on <a href="https://unsplash.com/@lvnatikk">Unsplash</a>
+      </div>
+    </div>
+    <a href="/places">
+        <button className="btn-primary">Places Page</button>
+    </a>
+</main>
+        </Def>
     )
-  }
+}
+
+
+
 
 module.exports = home
